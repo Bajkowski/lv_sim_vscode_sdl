170b2747df58bc0a49d798439ce908a3247dd3d3
bajkowski <kbajkowski@wisc.edu>
2021-10-16 20:25:27 -0500

    Start better code structure

--- main/src/main.c
+++ main/src/main.c
@@ -14,8 +14,6 @@
 #include <SDL2/SDL.h>
 #include "../../my_main.h"
 #include "lvgl/lvgl.h"
-#include "lvgl/examples/lv_examples.h"
-#include "lv_examples/lv_demo.h"
 #include "lv_drivers/display/monitor.h"
 #include "lv_drivers/indev/mouse.h"
 #include "lv_drivers/indev/keyboard.h"
@@ -78,30 +76,9 @@ int main(int argc, char **argv)
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
   hal_init();
 
-//  lv_example_switch_1();
-//  lv_example_calendar_1();
-//  lv_example_btnmatrix_2();
-//  lv_example_checkbox_1();
-//  lv_example_colorwheel_1();
-//  lv_example_chart_6();
-//  lv_example_table_2();
-//  lv_example_scroll_2();
-//  lv_example_textarea_1();
-//  lv_example_msgbox_1();
-//  lv_example_dropdown_2();
-//  lv_example_btn_1();
-//  lv_example_scroll_1();
-//  lv_example_tabview_1();
-//  lv_example_tabview_1();
-//  lv_example_flex_3();
-//  lv_example_label_1();
+  /* Calls original draw functions*/
+  start_ui();
 
-  //lv_demo_widgets();
-  lv_example_get_started_111();
-//  lv_demo_keypad_encoder();
-//  lv_demo_benchmark();
-//  lv_demo_stress();
-//  lv_demo_music();
 
   while(1) {
     /* Periodically call the lv_task handler.
--- my_main.h
+++ my_main.h
@@ -1,4 +1,3 @@
-
 /**
  * @file my_main.h
  *
@@ -13,6 +12,10 @@
 #include <SDL2/SDL.h>
 #include "lvgl/lvgl.h"
 
+// Main draw functions
+void start_ui();
+void draw_screen();
+void draw_main_widgets();
 
-void lv_example_get_started_111(void);
+// Button handlers
 void btn_event_cb(lv_event_t * e);
